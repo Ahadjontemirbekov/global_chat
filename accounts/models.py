ecf74a55e86ce819868f4473189682c140121211
Ahadjontemirbekov <ahadjontemirbekov149@gmail.com>
2026-06-15 01:17:36 +0500

    Add UserProfile model

---
+++ accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True, null=True)
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural = "User Profiles"
