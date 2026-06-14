1626515065e44a3b416066ebb5ab0c26fb404ee9
Ahadjontemirbekov <ahadjontemirbekov149@gmail.com>
2026-06-15 01:16:14 +0500

    Add Message model

---
+++ chat/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_edited = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.sender.username}: {self.content[:50]}'
